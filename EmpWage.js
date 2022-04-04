9f11ccd7e62a507f60a23342adc949e4a6efda58
Viney <vineykhaneja999@gmail.com>
2022-04-04 11:55:03 +0530

    [Viney] ADDED: Calculating EmpWage with PartTime also

--- EmpWage.js
+++ EmpWage.js
@@ -1,9 +1,11 @@
 console.log("Welcome to EmpWage Calculation Program in JS")
 //CONSTANTS
-const IS_PRESENT = 1;
+const IS_FULL_TIME = 2;
 const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
 const WAGE_PER_HR = 20;
 const FULL_DAY_HOURS = 8;
+const PART_TIME_HOURS = 4;
 
 //Variables
 let empWage;
@@ -14,12 +16,15 @@ console.log("Employee wage for this day is "+ empWage);
 //Method to get Work Hours
 function GetWorkingHrs(){
     let empCheck;
-    empCheck = Math.floor(Math.random() * 10) % 2;
+    empCheck = Math.floor(Math.random() * 10) % 3;
     switch(empCheck){
         case IS_ABSENT:
             return 0;
             break;
-        case IS_PRESENT:
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
             return FULL_DAY_HOURS;
             break;
     }
