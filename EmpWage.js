abf6388d9d92c8d61222d822e59267241fb62767
Viney <vineykhaneja999@gmail.com>
2022-04-04 11:45:17 +0530

    [Viney] ADDED: Calculating EmpWage

--- EmpWage.js
+++ EmpWage.js
@@ -2,16 +2,25 @@ console.log("Welcome to EmpWage Calculation Program in JS")
 //CONSTANTS
 const IS_PRESENT = 1;
 const IS_ABSENT = 0;
+const WAGE_PER_HR = 20;
+const FULL_DAY_HOURS = 8;
 
-//VARIABLES
-let empCheck;
+//Variables
+let empWage;
 
-empCheck = Math.floor(Math.random() * 10) % 2;
-switch(empCheck){
-    case IS_ABSENT:
-        console.log("Employee is absent");
-        break;
-    case IS_PRESENT:
-        console.log("Employee is present");
-        break;
+empWage = WAGE_PER_HR * GetWorkingHrs();
+console.log("Employee wage for this day is "+ empWage);
+
+//Method to get Work Hours
+function GetWorkingHrs(){
+    let empCheck;
+    empCheck = Math.floor(Math.random() * 10) % 2;
+    switch(empCheck){
+        case IS_ABSENT:
+            return 0;
+            break;
+        case IS_PRESENT:
+            return FULL_DAY_HOURS;
+            break;
+    }
 }
