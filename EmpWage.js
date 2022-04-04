11c3a550ebe8700834d8d26ff88e2bed998791ef
Viney <vineykhaneja999@gmail.com>
2022-04-04 11:38:24 +0530

    [Viney] ADDED: Checking Employee Present or Absent

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,17 @@
 console.log("Welcome to EmpWage Calculation Program in JS")
+//CONSTANTS
+const IS_PRESENT = 1;
+const IS_ABSENT = 0;
+
+//VARIABLES
+let empCheck;
+
+empCheck = Math.floor(Math.random() * 10) % 2;
+switch(empCheck){
+    case IS_ABSENT:
+        console.log("Employee is absent");
+        break;
+    case IS_PRESENT:
+        console.log("Employee is present");
+        break;
+}
